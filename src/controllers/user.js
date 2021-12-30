6bc89ef10018a698e2b9d75c8a0e028dce366aa7
Mario Santos <mariodev7@gmail.com>
2021-12-30 15:10:44 -0300

    feat(user): created test to verify signup

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -3,13 +3,40 @@ const UserModel = require("../db/User");
 class User {
   async signup(req, res) {
     try {
+      if (!req.body.name || req.body.name === "") {
+        return res
+          .status(400)
+          .json({ messageError: 'Field "Name" is missing.' });
+      }
+
+      if (!req.body.email || req.body.email === "") {
+        return res
+          .status(400)
+          .json({ messageError: 'Field "Email" is missing.' });
+      }
+
+      if (!req.body.password || req.body.password === "") {
+        return res
+          .status(400)
+          .json({ messageError: 'Field "Password" is missing.' });
+      }
+
       const { name, email, password } = req.body;
+
+      const hasExistUserWithEmail = await UserModel.findOne({ email });
+      if (hasExistUserWithEmail) {
+        return res
+          .status(400)
+          .json({ messageError: "Already exist user with it email." });
+      }
+
       const user = new UserModel({ name, email, password });
 
       await user.save();
 
       res.status(201).json(user);
     } catch (error) {
+      console.log(error);
       res.status(500).send(error);
     }
   }
--- test/user.test.js
+++ test/user.test.js
@@ -5,18 +5,72 @@ const faker = require("faker");
 const request = supertest(app);
 
 describe("SignUp User", () => {
-  it("Should sign up user with success", () => {
-    let name = faker.name.firstName();
-    let email = faker.internet.email();
-    let password = faker.internet.password();
-    let user = { name, email, password };
+  let name = faker.name.firstName();
+  let email = faker.internet.email();
+  let password = faker.internet.password();
+  let user = { name, email, password };
 
+  it("Should sign up user with success", () => {
     return request
       .post("/user")
       .send(user)
       .then((res) => {
         expect(res.statusCode).toEqual(201);
         expect(res.body.email).toEqual(email);
+      })
+      .catch((error) => {
+        throw new Error(error);
+      });
+  });
+
+  it("Should prevent sign up clone user", () => {
+    return request
+      .post("/user")
+      .send(user)
+      .then((res) => {
+        expect(res.statusCode).toEqual(400);
+        expect(res.body.messageError).toEqual(
+          "Already exist user with it email."
+        );
+      })
+      .catch((error) => {
+        throw new Error(error);
+      });
+  });
+
+  it("Should prevent sign up with empty 'name' field", () => {
+    return request
+      .post("/user")
+      .send({ email, password })
+      .then((res) => {
+        expect(res.statusCode).toEqual(400);
+        expect(res.body.messageError).toEqual('Field "Name" is missing.');
+      })
+      .catch((error) => {
+        throw new Error(error);
+      });
+  });
+
+  it("Should prevent sign up with empty 'email' field", () => {
+    return request
+      .post("/user")
+      .send({ name, password })
+      .then((res) => {
+        expect(res.statusCode).toEqual(400);
+        expect(res.body.messageError).toEqual('Field "Email" is missing.');
+      })
+      .catch((error) => {
+        throw new Error(error);
+      });
+  });
+
+  it("Should prevent sign up with empty 'password' field", () => {
+    return request
+      .post("/user")
+      .send({ email, name })
+      .then((res) => {
+        expect(res.statusCode).toEqual(400);
+        expect(res.body.messageError).toEqual('Field "Password" is missing.');
       })
       .catch((error) => {
         throw new Error(error);
