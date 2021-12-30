35f9cd404550ce591538c4c93deab720b7f9d517
Mario Santos <mariodev7@gmail.com>
2021-12-30 10:09:03 -0300

    feat(server): server listen in port 3030

---
+++ test/server.test.js
@@ -0,0 +1,14 @@
+const app = require("../src/app");
+const supertest = require("supertest");
+
+const request = supertest(app);
+
+it("Should listen in port 3030", () => {
+  return request
+    .get("/", (res) => {
+      expect(res.statusCode).toEqual(200);
+    })
+    .catch((error) => {
+      fail(error);
+    });
+});
