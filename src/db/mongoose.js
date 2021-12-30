284f6cd9231a87efbed14d051e32ee57a40fd80e
Mario Santos <mariodev7@gmail.com>
2021-12-30 10:40:31 -0300

    fix(mongoose db): remove console

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -2,5 +2,5 @@ const mongoose = require("mongoose");
 
 mongoose
   .connect("mongodb://localhost:27017/tddimg", { useNewUrlParser: true })
-  .then(() => console.log("Connected in database"))
+  .then(() => {})
   .catch((error) => console.error(error));
